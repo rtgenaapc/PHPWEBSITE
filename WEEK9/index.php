716b392e910430838a14019a83ecbfde9a8c7925
rtgenaapc <rtgena@apc.edu.ph>
2023-02-07 10:17:48 +0800

    b

--- WEEK9/index.php
+++ WEEK9/index.php
@@ -150,14 +150,9 @@ $dbname = "webprogmi211";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 
 // Create database
-$sql = "SELECT DATABASE webprogmi211";
+$sql = "UPDATE DATABASE webprogmi211";
 if ($conn->query($sql) === TRUE) {
   echo "Database created successfully";
 } else {
