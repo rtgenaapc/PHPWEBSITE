50589f8532d0d408d2acde79bac92870329a4d07
rtgenaapc <rtgena@apc.edu.ph>
2023-02-07 09:33:12 +0800

    test2

--- WEEK9/index.php
+++ WEEK9/index.php
@@ -145,14 +145,16 @@
 $servername = "localhost";
 $username = "webprogmi211";
 $password = "webprogmi211";
-$dbname = "webprogmi211";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
+?>
 
 //$name = $_POST['name'];
 //$email = $_POST['email'];
