3b4682b2eae81c86f4a22e96117738ddbdf0c799
rtgenaapc <rtgena@apc.edu.ph>
2023-02-07 10:58:58 +0800

    u

--- WEEK9/guests.php
+++ WEEK9/guests.php
@@ -1,23 +1,19 @@
-<//?php
+<?php
 $servername = "localhost";
-$username = "rtgena";
-$password = "rtgena";
-$dbname = "Website";
+$username = "webprogmi211";
+$password = "webprogmi211";
+$dbname = "webprogmi211";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
-$sql = "SELECT id, Lastname, email, website, comment FROM MyGuests";
+$sql = "SELECT id, Fullname, email, website, comment FROM MyGuests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["Lastname"]. "  - Email: " . $row["email"]. "  - Website: " . $row["website"]. "  - Comment: " . $row["comment"]."<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["Fullname"]. "  - Email: " . $row["email"]. "  - Website: " . $row["website"]. "  - Comment: " . $row["comment"]."<br>";
   }
 } else {
   echo "0 results";
